d2789b950bbf5916a3c52a147060f3da79955bb2
Snehil <51155558+SneakySensei@users.noreply.github.com>
2022-06-05 16:16:02 +0530

    Time: 1269 ms (6.68%), Space: 52.1 MB (6.65%) - LeetHub

--- 128-longest-consecutive-sequence/128-longest-consecutive-sequence.cpp
+++ 128-longest-consecutive-sequence/128-longest-consecutive-sequence.cpp
@@ -6,21 +6,16 @@ public:
             m[x] = true;
         }
         
-        vector<vector<int>> seq;
+        int max = 0;
         for(auto x: nums){
             // Detect start of a sequence
             if(!m[x-1]){
-                vector<int> temp = {x};
-                for(int i = x+1; m[i]; i++){
-                    temp.push_back(i);
+                int i = x+1;
+                while(m[i]){
+                    i++;
                 }
-                seq.push_back(temp);
+                max = i-x > max ? i-x : max;
             }
-        }
-        
-        int max = 0;
-        for(auto s: seq){
-            max = s.size() > max ? s.size() : max;
         }
         return max;
     }
